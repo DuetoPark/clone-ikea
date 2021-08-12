d851967fdb115d35e6ef8f37f8ff3c0846cae76a
DuetoPark <inhye.park.dev@gmail.com>
2021-08-12 11:07:29 +0900

    💄 Implement the carousel-modal component

--- js/_product-carousel.js
+++ js/_product-carousel.js
@@ -6,3 +6,13 @@ var productCarousel = tns({
   navContainer: '.product-carousel .pagenation',
   navPosition: 'bottom',
 })
+
+var productCarouselModal = tns({
+  container: '.carousel-modal .slider-list',
+  items: 1,
+  loop: false,
+  controlsContainer: '.carousel-modal .slider-controls',
+  mode: 'gallery',
+  navContainer: '.carousel-modal .pagenation',
+  navPosition: 'bottom',
+})
