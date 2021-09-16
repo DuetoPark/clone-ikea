cdc102ddf03bfb1f8eedd79195be71d0785fec2b
DuetoPark <inhye.park.dev@gmail.com>
2021-09-16 22:25:46 +0900

    ✨ InitialIze recommandationsSlider

---
+++ js/_slider.js
@@ -0,0 +1,11 @@
+var recommandationsSlider = tns({
+  container: '.recommandations .slider-list',
+  items: 2.5,
+  loop: false,
+  controlsContainer: '.recommandations .slider-controls',
+  navContainer: '.recommandations .pagenation',
+  navPosition: 'bottom',
+  gutter: '20',
+  slideBy: 2,
+  edgePadding: 10,
+})
