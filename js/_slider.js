7cca81624825299533ed35aac411a9e264b31efd
DuetoPark <inhye.park.dev@gmail.com>
2021-09-20 00:34:02 +0900

    💄 Implement slider-group components

--- js/_slider.js
+++ js/_slider.js
@@ -19,3 +19,66 @@ var recommandationsSlider = tns({
     },
   },
 })
+
+const withSlider = tns({
+  container: '.slider-group .with .slider-list',
+  items: 2.5,
+  loop: false,
+  controlsContainer: '.slider-group .with .slider-controls',
+  gutter: '20',
+  slideBy: 2,
+  edgePadding: 10,
+  responsive: {
+    600: {
+      items: 3.43,
+      slideBy: 3,
+      edgePadding: 32,
+    },
+    900: {
+      items: 4.1,
+      slideBy: 4,
+    },
+  },
+})
+
+const matchSlider = tns({
+  container: '.slider-group .match .slider-list',
+  items: 2.5,
+  loop: false,
+  controlsContainer: '.slider-group .match .slider-controls',
+  gutter: '20',
+  slideBy: 2,
+  edgePadding: 10,
+  responsive: {
+    600: {
+      items: 3.43,
+      slideBy: 3,
+      edgePadding: 32,
+    },
+    900: {
+      items: 4.1,
+      slideBy: 4,
+    },
+  },
+})
+
+const moreSlider = tns({
+  container: '.slider-group .more .slider-list',
+  items: 2.5,
+  loop: false,
+  controlsContainer: '.slider-group .more .slider-controls',
+  gutter: '20',
+  slideBy: 2,
+  edgePadding: 10,
+  responsive: {
+    600: {
+      items: 3.43,
+      slideBy: 3,
+      edgePadding: 32,
+    },
+    900: {
+      items: 4.1,
+      slideBy: 4,
+    },
+  },
+})
