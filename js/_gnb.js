84a6c379b61d1c090dd59474b94c6f10add54672
DuetoPark <inhye.park.dev@gmail.com>
2021-09-22 02:13:18 +0900

    ✨ Add gnb scroll event

---
+++ js/_gnb.js
@@ -0,0 +1,50 @@
+const gnb = document.querySelector('.gnb');
+const main = document.querySelector('main');
+
+let lastScrollTop = 0;
+const coords = {
+  gnbTop: gnb.offsetTop,
+  gnbBottom: gnb.offsetTop + gnb.offsetHeight,
+};
+
+function fixGNB() {
+  gnb.classList.add('is-fixed');
+  main.style.paddingTop = gnb.offsetHeight + 'px';
+}
+
+function breakGNB() {
+  gnb.classList.remove('is-fixed');
+  gnb.classList.remove('is-up');
+  gnb.classList.remove('is-down');
+  main.style.paddingTop = '0px';
+}
+
+function downScrollGNB() {
+  gnb.classList.add('is-down');
+  gnb.classList.remove('is-up');
+}
+
+function upScrollGNB() {
+  gnb.classList.add('is-up');
+  gnb.classList.remove('is-down');
+}
+
+function scrollHandler() {
+  let scrollTop = window.pageYOffset;
+
+  if (scrollTop > coords.gnbBottom) {
+    fixGNB();
+
+    if (scrollTop > lastScrollTop) {
+      downScrollGNB();
+    } else {
+      upScrollGNB();
+    }
+  } else if (scrollTop <= coords.gnbTop) {
+    breakGNB();
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+}
+
+window.addEventListener('scroll', scrollHandler);
